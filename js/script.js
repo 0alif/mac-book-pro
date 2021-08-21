eef50f3360ae3212e20fa26716386ba32699a54d
0alif <alifhossain003@gmail.com>
2021-08-21 13:13:51 +0600

    promoCode

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-
-
 function extraCost(idName, value) {
     const previousCost = document.getElementById(idName);
     previousCost.innerText = value;
 }
+
+function promoCode(inputText) {
+    if (inputText.value == 'stevekaku') {
+        const subTotalElement = document.getElementById('total-price');
+        const totalElement = document.getElementById('total');
+
+        const subTotal = subTotalElement.innerText;
+        const grandTotal = subTotal - (subTotal * 20 / 100);
+        totalElement.innerText = grandTotal;
+    }
+    inputText.value = '';
+}
+
 
 
 
@@ -38,5 +49,35 @@ document.getElementById('paidDelivery').addEventListener('click', function () {
     extraCost('delivery-charge', 20);
 });
 
-// Pomo code
+// total price
+
+/* const mainCostText = document.getElementById('best-price').innerText;
+const mainCost = parseInt(mainCostText);
+
+const memoryCostText = document.getElementById('memory-cost').innerText;
+const memoryCost = parseInt(memoryCostText);
+
+const storageCostText = document.getElementById('storage-cost').innerText;
+const storageCost = parseInt(storageCostText);
+
+const deliveryCostText = document.getElementById('delivery-charge').innerText;
+const deliveryCost = parseInt(deliveryCostText);
+
+let currentBalance = document.getElementById('best-price').innerText;
+let totalBalance = document.getElementById('total-price').innerText;
+totalBalance.innerText = mainCost + memoryCost + storageCost + deliveryCost + parseInt(currentBalance);
+ */
+// console.log(totalPrice);
+// document.getElementById('total-price').innerText = totalPrice;
+// document.getElementById('total').innerText = totalPrice;
+
+
+
+
+
+// Promo code
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const pomoCode = document.getElementById('pomo-code');
+    promoCode(pomoCode);
+});
 
